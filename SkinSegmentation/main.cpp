db42909806714bb92bca663a812516242ff9b785
Sasha Nicolas <sasha@laws.deinf.ufma.br>
2014-10-27 16:59:45 -0200

    Correcao do c means

--- SkinSegmentation/main.cpp
+++ SkinSegmentation/main.cpp
@@ -12,16 +12,16 @@
 #include <sstream>
 #include <cmath>
 
+#define NUM_CLASSES 5
+#define ERROR_FCM 0.0005
+#define FUZZIFIER 2
+
 using namespace std;
 
 struct color{
-    unsigned short r, g, b, y;
+    float r, g, b, y;
 };
 typedef struct color Color;
-
-#define NUM_CLASSES 5
-#define ERROR_FCM 0.8
-#define FUZZIFIER 2
 
 struct vec3 {
     float x, y, z;
@@ -35,10 +35,22 @@ float norma(Vec3 a, Vec3 b){
             pow(a.z - b.z, 2)
         );
 }
+float normaN(vector<float> a, vector<float> b){
+    float soma=0;
+    for (int i=0; i<a.size(); i++) {
+        soma += pow(a.at(i) - b.at(i), 2);
+    }
+    return sqrt( soma );
+}
 
 int main(int argc, const char * argv[]) {
     ifstream dataset_file("Skin_NonSkin.txt", ios::in);
     vector< Color > data;
+    
+//    vector<float> a = {3,0};
+//    vector<float> b = {0,4};
+//    cout << normaN(a,b) << endl;
+//    return 0;
     
     //abrir o arquivo
     if(dataset_file){
@@ -46,8 +58,8 @@ int main(int argc, const char * argv[]) {
         //listar os pontos RGB do arquivo
         
         string line;
-        int i=2000;
-        while( getline(dataset_file, line) && i--)
+//        int i=50000;
+        while( getline(dataset_file, line))
         {
             Color cor;
             istringstream is( line );
@@ -55,28 +67,43 @@ int main(int argc, const char * argv[]) {
             //cout << cor.b << " " << cor.g << " " << cor.r << " " << cor.y << endl;
             data.push_back(cor);
         }
-        
-        /*vector< Color >  new_;
-        new_.swap(data);
-        cout << data.size() << endl;
-        for(int i = 0; i<new_.size();i++)
-        {
-            cout << new_.at(i).b << " " << new_.at(i).g << " " << new_.at(i).r << " " << new_.at(i).y << endl;
-        }//*/
-        
     }else{ //if ler o aquivo
         cout << "Nao achou o arquivo\n";
         return 0;
     }//if ler o aquivo
+    //*/
+        
+    //teste com pontos controlados
+    /*for (int i=0; i<10; i++) {
+        Color cor;
+        cor.r = sin((36 * i)*M_PI/180);
+        cor.g = cos((36 * i)*M_PI/180);
+        cor.b = 0;
+        data.push_back(cor);
+    }
+    for (int i=0; i<10; i++) {
+        Color cor;
+        cor.r = sin((36 * i)*M_PI/180);
+        cor.g = cos((36 * i)*M_PI/180);
+        cor.b = 10;
+        data.push_back(cor);
+    }
+    for(int i = 0; i<20; i++)
+    {
+        
+        cout << "r=" << data.at(i).r << " g=" << data.at(i).g << " b=" << data.at(i).b << endl;
+        
+    }//*/
+    //end - teste com pontos controlados
     
-    //arquivo carregado em data
+    //end - arquivo carregado em data
     
     //Fuzzy c-means (FCM)
     int N = (int) data.size();
     vector< vector<float> > U; //matriz de graus de associacao (degree of membership)
     vector< vector<float> > U_anterior;
     
-    //instanciar com valores aleatorios a matriz U
+    //instanciar com valores aleatorios a matriz U, probabilidades de ser do cluster j
     //passo 1
     for (int i=0; i<N ; i++) {
         vector<float> novo;
@@ -93,6 +120,7 @@ int main(int argc, const char * argv[]) {
     
     while (!convergiu) {
         cout << step_k << endl;
+        C.clear();
         //passo 2 - calcular os centróides de cada NUM_CLASSES cluster
         for (int j=0; j < NUM_CLASSES; j++) {
             //calculo do denominador - soma da coluna j
@@ -115,7 +143,22 @@ int main(int argc, const char * argv[]) {
             centroid.push_back(soma_Cb/soma_dem);
             C.push_back(centroid);
         }
+        
+        cout << "Imprime os U:"<<endl;
+        for(int i = 0; i<50000; i+=2000)
+        {
+            for(int j = 0; j<NUM_CLASSES; j++)
+            {
+                
+                cout << U.at(i).at(j) << " ";
+            }
+            cout << endl;
+            cout << "ponto corr: b=" << data.at(i).b << " g=" << data.at(i).g << " r=" << data.at(i).r << endl;
+
+        }//*/
+        
         //passo 3 - update de U(k), U(k+1)
+        U_anterior.clear();
         U_anterior.swap(U);
         float expoente = 2./(FUZZIFIER-1);
         
@@ -136,25 +179,19 @@ int main(int argc, const char * argv[]) {
             }
         }
         
-        /*for(int i = 0; i<10; i++)
+        cout << "Imprime os centros:"<<endl;
+        for(int i = 0; i<NUM_CLASSES; i++)
         {
-            for(int j = 0; j<5; j++)
-            {
-                cout << U.at(i).at(j) << " " << U_anterior.at(i).at(j) << endl;
-            }
+            cout << "r=" << C.at(i).at(0) << " g=" << C.at(i).at(1) << " b=" << C.at(i).at(2) << endl;
+            
         }
         cout << endl;//*/
         
         //for for IF error
         bool algum_maior = false;
         for (int i=0; i<N ; i++) {
-            for (int j=0; j < NUM_CLASSES; j++) {
-                if(abs(U.at(i).at(j) - U_anterior.at(i).at(j)) > ERROR_FCM){
-                    algum_maior = true;
-                    break;
-                }
-            }
-            if(algum_maior){
+            if( normaN (U.at(i), U_anterior.at(i))  > ERROR_FCM){
+                algum_maior = true;
                 break;
             }
         }
@@ -162,7 +199,12 @@ int main(int argc, const char * argv[]) {
         
         step_k++;
     }
-    
+    cout << "Imprime os centros finais:"<<endl;
+    for(int i = 0; i<NUM_CLASSES; i++)
+    {
+        cout << "r=" << C.at(i).at(0) << " g=" << C.at(i).at(1) << " b=" << C.at(i).at(2) << endl;
+        
+    }
     return 0;
 }
 
